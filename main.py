25c67fed430b88848acf9fb46761f8d89bdfde50
Charlie Edwards <285464439+Charlie-Edwards@users.noreply.github.com>
2026-07-19 18:26:08 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# This has nothing to do with the actual CTF itself, its just to validate answers.
+
+import sys, hashlib
+
+flag_hashes = {
+    "puzzle 1": "fe6a30203e4f5f5a94ab8fb6f8535e2630926f242b6fbd5aae296922a2beaa53",
+}
+
+try:
+    userguess = sys.argv[1]
+    guess_hash = hashlib.sha256(userguess.encode()).hexdigest()
+
+    flag = next((k for k, v in flag_hashes.items() if v == guess_hash), None)
+
+    if flag:
+        print(f"Well done, you found the flag for {flag} ({userguess})!")
+    else:
+        print(f"{userguess} doesn't match any flag")
+
+except IndexError:
+    print("Incorrect usage (eg, \"python3 check.py flag{EXAMPLE}\")")
+
+# Thank you for trying!!!
